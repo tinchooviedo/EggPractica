70c456da79b7ab8dec57e3c8555c5c5c427def05
Fernando Cocco <fernandoagustincocco@gmail.com>
2020-12-01 09:46:42 -0300

    Commit del Fer

---
+++ src/relacionespoo/Pruieba.java
@@ -0,0 +1,19 @@
+package relacionespoo;
+
+public class Pruieba {
+
+    private String titulo;
+
+    public Pruieba(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+}
